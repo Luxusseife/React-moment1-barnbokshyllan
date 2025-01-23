dc903af6a86fb4388cfe7ec95ab63067259069dd
Jenny Lind <jeli2308@student.miun.se>
2025-01-23 16:26:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import BookCard from './components/BookCard'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <Header />
+    <BookCard/>
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/BookCard.tsx
@@ -0,0 +1,17 @@
+interface BookInfo {
+    author: string,
+    illustrator: string,
+    publisher: string,
+    publicated: number,
+    read: boolean
+}
+
+function Book({author, illustrator, publisher, publicated, read} : BookInfo) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Book
